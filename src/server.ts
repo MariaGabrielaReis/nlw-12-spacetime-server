8938772b95b1693844eb0a614dc850c1921c2cb5
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-05-15 21:52:00 -0300

    chore: configurate database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.get("/hi", () => "hi!");
+app.get("/users", async () => {
+  const users = await prisma.user.findMany();
+  return users;
+});
 
 app
-	.listen({ port: 3333 })
-	.then(() => console.log("🔥 server is running on http://localhost:3333"));
+  .listen({ port: 3333 })
+  .then(() => console.log("🔥 server is running on http://localhost:3333"));
